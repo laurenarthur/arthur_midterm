ab50a4899979c640bed003c907bfa4ad8b1f4350
laurenarthur <arthur_lauren@columbusstate.edu>
2020-10-17 14:24:01 +0000

    Commit is being added but random is not working

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var express = require('express');
 //fetch
 var fetch = require('node-fetch');
 
+//body-parser
+var bodyParser = require("body-parser");
+
 //call express
 var app = express();
 
@@ -15,24 +18,41 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 //make styles publiv
 app.use(express.static("public"));
-
+//tell app to use Body parser
+app.use(bodyParser.urlencoded({extended: true}));
 
 //get home page / 
 app.get('/', function(req, res){
-    res.render('index');
+    //let comicData;
+    fetch('http://xkcd.com/info.0.json')
+    .then(res => res.json())
+    .then(data => {
+        res.render('index',{data:data});
+    });
+    console.log("got it");
+    //res.render('index');
 });
 
 
 //fetch comic of the day 
-app.get('/comic', function(req, res){
-    let comicdata;
-    fetch('')
+//app.get('/comic', function(req, res){
+    //fetch('http://xkcd.com/info.0.json')
+   // .then(res => res.json())
+   // .then(data => {
+     //   res.render('comic',{data:data});
+   // });
+//})
+
+//fetch random comic
+app.get('/random', function(req, res){
+    let randnumber = Math.floor(Math.random() * 2372);  
+    fetch('http://xkcd.com/info.0.json' + randnumber)
     .then(res => res.json())
     .then(data => {
-        comicdata = data;
-        res.json(comicdata);
+        res.render('random', {data:data});
     });
 })
+
 //server setup 
 app.listen(port, function(){
     console.log('Listening on ' + port)
